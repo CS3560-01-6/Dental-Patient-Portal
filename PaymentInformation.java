19cea4d51f1c4f4f1ce49b9df5807ea1cad869f4
phillipche-cpp <pche@cpp.edu>
2022-03-29 20:12:19 -0700

    Added setter and getter methods to PaymentInformation

--- PaymentInformation.java
+++ PaymentInformation.java
@@ -5,11 +5,64 @@ public class PaymentInformation
     private String expDate;
     private int securityCode;
 
-    public PaymentInformation(int cardNuber, String nameOncard, String expDate, int securityCode)
+    public PaymentInformation(int cardNumber, String nameOncard, String expDate, int securityCode)
     {
-        this.cardNumber = cardNuber;
+        this.cardNumber = cardNumber;
         this.nameOnCard = nameOncard;
         this.expDate = expDate;
         this.securityCode = securityCode;
+    }
+
+    public void updatePayment(int cardNumber, String nameOncard, String expDate, int securityCode)
+    {
+        this.cardNumber = cardNumber;
+        this.nameOnCard = nameOncard;
+        this.expDate = expDate;
+        this.securityCode = securityCode;
+    }
+
+    public void updateCardName(int cardNumber)
+    {
+        this.cardNumber = cardNumber;
+    }
+
+    public void updatePaymentName(String nameOnCard)
+    {
+        this.nameOnCard = nameOnCard;
+    }
+
+    public void updateExpDate(String expDate)
+    {
+        this.expDate = expDate;
+    }
+
+    public void updateSecurityCode(int securityCode)
+    {
+        this.securityCode = securityCode;
+    }
+
+    public int getCardNumber()
+    {
+        return cardNumber;
+    }
+
+    public String getCardName()
+    {
+        return nameOnCard;
+    }
+
+    public String getExpDate()
+    {
+        return expDate;
+    }
+
+    public int getSecurityCode()
+    {
+        return securityCode;
+    }
+
+    public String toString()
+    {
+        return nameOnCard + "\n" + cardNumber + "\n" + expDate + "\t" + securityCode + "\n";
     }
 }
