c6d1e42b851286c0e0988f673b739d61721682f9
phillipche-cpp <pche@cpp.edu>
2022-04-07 01:29:12 -0700

    Added toString() Method in Address class

--- Address.java
+++ Address.java
@@ -5,14 +5,17 @@ public class Address
     private String city;
     private String state;
     private int zip;
-    private String country;
 
-    public Address(String addressLine1,String addressLine2, String city, String state, int zip, String country)
+    public Address(String addressLine1,String addressLine2, String city, String state, int zip)
     {
         this.addressLine1 = addressLine1;
         this.addressLine2 = addressLine2;
         this.city = city;
         this.zip = zip;
-        this.country = country;
+    }
+
+    public String toString()
+    {
+        return addressLine1 + ", " + addressLine2 + "\n" + city + "\t" + state + "\n" + zip + "\n";
     }
 }
