a80c04985ff7aa89b521f2c857cc15d35fa7036e
phillipche-cpp <pche@cpp.edu>
2022-04-22 16:32:52 -0700

    Updated Home Page Layout

--- homePage.java
+++ homePage.java
@@ -3,23 +3,60 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Tab;
+import javafx.scene.text.Text;
 
-public class homePage {
+public class HomePage {
+
+    @FXML
+    private Text addressLine1;
+
+    @FXML
+    private Text addressLine2;
+
+    @FXML
+    private Text city;
+
+    @FXML
+    private Text dateOfBirth;
+
+    @FXML
+    private Text email;
+
+    @FXML
+    private Text firstName;
 
     @FXML
     private Tab invoicesTab;
 
     @FXML
+    private Text lastFourDigits;
+
+    @FXML
+    private Text lastName;
+
+    @FXML
     private Button logoutButton;
+
+    @FXML
+    private Text patientID;
 
     @FXML
     private Tab paymentHistoryTab;
 
     @FXML
+    private Text phoneNumber;
+
+    @FXML
     private Tab profileTab;
 
     @FXML
+    private Text state;
+
+    @FXML
     private Hyperlink updateProfileButton;
+
+    @FXML
+    private Text zip;
 
     @FXML
     void logout(ActionEvent event) {
