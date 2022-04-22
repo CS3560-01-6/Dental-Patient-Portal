4a23bc45054a4fd34e01bea107d6890e1003307b
phillipche-cpp <pche@cpp.edu>
2022-04-22 16:47:47 -0700

    Added Update Profile Controller Class

---
+++ UpdateProfilePage.java
@@ -0,0 +1,45 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class UpdateProfilePage {
+
+    @FXML
+    private TextField CVV;
+
+    @FXML
+    private TextField addressLine1;
+
+    @FXML
+    private TextField addressLine2;
+
+    @FXML
+    private TextField cardHolder;
+
+    @FXML
+    private TextField cardNumber;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField expiryDate;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private TextField state;
+
+    @FXML
+    private TextField zip;
+
+}
