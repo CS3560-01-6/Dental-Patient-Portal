15f7fda7a1da634cef855e0466603ccc370d8f12
mikephu <mikephu03@gmail.com>
2022-04-25 20:40:00 -0700

    Added try/catch statement to LoginPage.java / removed methods from Handler.java

--- LoginPage.java
+++ LoginPage.java
@@ -32,27 +32,33 @@ public class LoginPage {
 
     /* Checks the login using Patient ID and Password. */
      private void checkLogin() throws Exception {
-        App app = new App();
-        Handler sqlConnection = new Handler();
-        connection = sqlConnection.connectDB();
+        try
+        {
+            App app = new App();
+            Handler sqlConnection = new Handler();
+            connection = sqlConnection.connectDB();
 
-        String verifyLoginQuery = "SELECT count(1) FROM patient WHERE patientID = '" + patientID.getText() + "' AND password = '" + password.getText() + "'";
+            String verifyLoginQuery = "SELECT count(1) FROM patient WHERE patientID = '" + patientID.getText() + "' AND password = '" + password.getText() + "'";
 
-        Statement statement = connection.createStatement();
-        ResultSet queryResult = statement.executeQuery(verifyLoginQuery);
+            Statement statement = connection.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLoginQuery);
 
-        while(queryResult.next()) {
-            if(queryResult.getInt(1) == 1) {
-                System.out.println("Patient logged in successfully.");
-                app.changeScene("HomeScene.fxml");
-            } else if (patientID.getText().isEmpty() || password.getText().isEmpty()) {
-                wrongLogin.setText("Missing required login fields.");
-            } else {
-                wrongLogin.setText("Invalid Patient ID or Password.");
+            while(queryResult.next()) {
+                if(queryResult.getInt(1) == 1) {
+                    System.out.println("Patient logged in successfully.");
+                    app.changeScene("HomeScene.fxml");
+                } else if (patientID.getText().isEmpty() || password.getText().isEmpty()) {
+                    wrongLogin.setText("Missing required login fields.");
+                } else {
+                    wrongLogin.setText("Invalid Patient ID or Password.");
+                }
             }
         }
+        catch(Exception e)
+        {
+            System.out.println("Error. Login Failed. " + e.getMessage());
+        }
     }
-
 }
 
 
