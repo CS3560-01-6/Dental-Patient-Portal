f87a30bc504ef6edd87e10f9b9ddd163fbb4b326
phillipche-cpp <pche@cpp.edu>
2022-04-27 22:54:46 -0700

    Implemented Controller Class for InvoicePage

--- InvoicePage.java
+++ InvoicePage.java
@@ -1,3 +1,55 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
 public class InvoicePage {
-    
+
+    @FXML
+    private Text invoiceID;
+
+    @FXML
+    private Text invoicePaymentDueDate;
+
+    @FXML
+    private Text invoiceStatus;
+
+    @FXML
+    private Label invoiceTotal;
+
+    @FXML
+    private Label patientAddress;
+
+    @FXML
+    private Label patientEmail;
+
+    @FXML
+    private Label patientName;
+
+    @FXML
+    private Label patientPhone;
+
+    @FXML
+    private Button payButton;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private Label treatmentCost;
+
+    @FXML
+    private Label treatmentName;
+
+    @FXML
+    void payInvoice(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnToInvoiceList(ActionEvent event) {
+
+    }
+
 }
